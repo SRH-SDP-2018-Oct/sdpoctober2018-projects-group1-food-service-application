a851db2674b1ed7c4c9de76084ab54daf858f181
jisu park <qkrwltn8976@naver.com>
2018-11-28 16:44:37 +0100

    Revert "Create Page.java"
    This reverts commit 121656f4f3e56586f0d955d598fcce87595210c2.

--- Class/Cal.java
+++ Class/Cal.java
@@ -13,18 +13,20 @@ public class Cal {
 	public static void PrintCalendar() {
 		c.set(Calendar.YEAR, Integer.valueOf(year));
 		c.set(Calendar.MONTH, Integer.valueOf(month)-1);
-		
+	
+		c.set(Calendar.DAY_OF_MONTH,1);
 		int dayofweek = c.get(Calendar.DAY_OF_WEEK);
 		c.set(Calendar.DAY_OF_MONTH,32);
 		int lastday = 32-c.get(Calendar.DAY_OF_MONTH);
 		
 		System.out.println("S\tM\tT\tW\tT\tF\tS");
-		for(int i=0; i<dayofweek-1; i++)
+		int i=0;
+		for(; i<dayofweek-1; i++)
 			System.out.print("\t");
-		for(int i=0,j=0; j<=lastday;i++,j++) {
+		for(int day=1; day<=lastday;day++,i++) {
 			if(i%7==0)
 				System.out.println("");
-			System.out.print(j+"\t");
+			System.out.print(day+"\t");
 		}
 	}
 	public static void ChooseDate() {
@@ -36,6 +38,9 @@ public class Cal {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("\n\nSelect Date>>");
 		day = sc.next();
+	}
+	public static Date getDate() {
+		return date;
 	}
 	public static void ShowCalendar() throws ParseException {
 		System.out.print("\033[H\033[2J");
